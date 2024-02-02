2d6cb91eeca0cc6e916686640a7e6207e90b94cd
ilyanosovsky <imnosovsky@gmail.com>
2024-02-02 14:25:59 +0200

    step 52

--- 03 Music Player/script.js
+++ 03 Music Player/script.js
@@ -119,6 +119,16 @@ const playNextSong = () => {
   }
 };
 
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) {
+    return;
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+    playSong(previousSong.id);
+  }
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -151,6 +161,10 @@ playButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
+
+previousButton.addEventListener("click", playPreviousSong);
 
 userData?.songs.sort((a, b) => {
   if (a.title < b.title) {
