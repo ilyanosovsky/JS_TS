e37ae65d167abbee011cc67800bb7cdf6537cc4c
ilyanosovsky <imnosovsky@gmail.com>
2024-02-03 15:13:56 +0200

    step 69

--- 03 Music Player/script.js
+++ 03 Music Player/script.js
@@ -100,6 +100,9 @@ const playSong = (id) => {
 
   userData.currentSong = song;
   playButton.classList.add("playing");
+  highlightCurrentSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
   audio.play();
 };
 
@@ -129,6 +132,32 @@ const playPreviousSong = () => {
   }
 };
 
+const shuffle = () => {
+    userData?.songs.sort(() => Math.random() - 0.5);
+}
+
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+  if (songToHighlight) {
+    songToHighlight.setAttribute("aria-current", "true");
+  }
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -147,6 +176,11 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+const setPlayButtonAccessibleText = () => {
+    const song = userData?.currentSong || userData?.songs[0];
+    playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play"); 
+}
 
 const getCurrentSongIndex = () => {
   return userData?.songs.indexOf(userData?.currentSong);
