1efa07902a4625852bc61992677affa66ba26510
ilyanosovsky <imnosovsky@gmail.com>
2024-02-05 20:25:55 +0200

    start of pr

---
+++ 05 Polindrome/script.js
@@ -0,0 +1,14 @@
+const checkButton = document.getElementById("check-btn");
+const textInput = document.getElementById("text-input");
+const resultDiv = document.getElementById("result");
+
+
+checkButton.addEventListener("click", () => {
+  if (textInput.textContent === "") {
+    alert("Please input a value");
+    console.log("empty button was clicked");
+  } else if (textInput.textContent === "A") {
+    resultDiv.innerText = "A is a palindrome";
+    console.log("A button was clicked");
+  }
+});
