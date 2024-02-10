9280146a85b0cdeee0d921ec2c929491e2a619ba
ilyanosovsky <imnosovsky@gmail.com>
2024-02-10 14:24:54 +0200

    mute log

--- 05 Polindrome/script.js
+++ 05 Polindrome/script.js
@@ -5,10 +5,10 @@ const resultDiv = document.getElementById("result");
 
 checkButton.addEventListener("click", () => {
   if (textInput.textContent === "") {
-    alert("Please input a value");
+    // alert("Please input a value");
     console.log("empty button was clicked");
   } else if (textInput.textContent === "A") {
     resultDiv.innerText = "A is a palindrome";
-    console.log("A button was clicked");
+    // console.log("A button was clicked");
   }
 });
